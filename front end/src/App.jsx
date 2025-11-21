075df325da8d77e689cc9bd259cd64f2f6d0b054
George Bahij <georgesoukariah@gmail.com>
2025-11-21 16:28:54 +0200

    added login webpage component (empty for now)

---
+++ front end/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./app.css"
+
+function App(){
+  return(
+    <h1 className="bg-green-700">Hello World</h1>
+  )
+}
+
+export default App;
---
+++ front end/src/main.jsx
@@ -0,0 +1,14 @@
+// render app component for the html file
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+
+
