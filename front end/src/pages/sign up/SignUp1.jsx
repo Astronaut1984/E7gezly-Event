3ca38f27a9fe2a1659f2a8f8306cfd0a0890c4db
bradocola <nagyomar91@gmail.com>
2025-11-27 19:08:26 +0200

    Sign up update

---
+++ front end/src/pages/sign up/SignUp1.jsx
@@ -0,0 +1,92 @@
+import "./signup.css";
+import Input from "../../components/Input";
+import SelectType from "../../components/selectType";
+
+export default function SignUp1() {
+  const optionsAcc = ["Attendee", "Organizer"];
+  return (
+    <>
+      <div className="flex gap-10">
+        <Input
+          title="First Name"
+          type="text"
+          placeholder="Type your first name"
+          name="firstName"
+        />
+        <Input
+          title="Last Name"
+          type="text"
+          placeholder="Type your last name"
+          name="lastName"
+        />
+      </div>
+      <div className="flex gap-10">
+        <Input
+          title="Email"
+          type="text"
+          placeholder="Type your Email"
+          name="email"
+        />
+        <SelectType
+          title="Account Type"
+          type="text"
+          options={optionsAcc}
+          name="accountType"
+          classNameVar="w-[50px]" //we need to review this again
+        />
+      </div>
+    </>
+  );
+}
+
+export function SignUp2() {
+  return (
+    <>
+      <div className="flex gap-10">
+        <Input
+          title="Country"
+          type="text"
+          placeholder="Select your country"
+          name="country"
+        />
+        <Input
+          title="City"
+          type="text"
+          placeholder="Select your city"
+          name="city"
+        />
+      </div>
+      <Input
+        title="Phone Number"
+        type="digit"
+        placeholder="Enter your phone"
+        name="phoneNumber"
+      />
+    </>
+  );
+}
+
+export function SignUp3() {
+  return (
+    <>
+      <Input
+        title="Username"
+        type="text"
+        placeholder="Type a username"
+        name="username"
+      />
+      <Input
+        title="Password"
+        type="password"
+        placeholder="Type a password"
+        name="password"
+      />
+      <Input
+        title="Reconfirm Password"
+        type="password"
+        placeholder="Re-type a password"
+        name="rePassword"
+      />
+    </>
+  );
+}
--- front end/src/pages/sign up/signup.jsx
+++ front end/src/pages/sign up/signup.jsx
@@ -1,97 +1,40 @@
 import "./signup.css";
 import Input from "../../components/Input";
 import SelectType from "../../components/selectType";
+import { useState } from "react";
+import SignUp1, { SignUp2, SignUp3 } from "./SignUp1";
 
 const optionsAcc = ["Attendee", "Organizer"];
 
 export default function SignUp() {
-  let bgColor = "bg-gradient-to-tr from-blue-200 via-blue-400 to-blue-600";
+  const bgColor = "bg-gradient-to-tr from-blue-200 via-blue-400 to-blue-600";
+  const [pageNumber, setPageNumber] = useState(1);
 
+  function nextPageHandeler() {
+    setPageNumber((currPageNumber) => currPageNumber + 1);
+  }
   return (
     <main
       className={`${bgColor} flex justify-center items-center w-full min-h-screen`}
     >
-      <div className="w-[900px] bg-white rounded-[100px] p-[65px]">
+      <div className="w-[500px] bg-white rounded-[10px] p-[65px]">
         <form className="w-full">
           <span className="block font-bold text-[39px] text-[#333333] leading-[1.2] text-center mb-10 text-blue-500">
             E7gezly Event Create Account
           </span>
-          <div className="flex gap-10">
-            <Input
-              title="First Name"
-              type="text"
-              placeholder="Type your first name"
-              name="firstName"
-              classNameVar="ml-[20px]"
-            />
-            <Input
-              title="Last Name"
-              type="text"
-              placeholder="Type your last name"
-              name="lastName"
-            />
-            <Input
-              title="Phone Number"
-              type="digit"
-              placeholder="Enter your phone"
-              name="phoneNumber"
-              classNameVar="ml-[140px] mr-[20px]"
-            />
-          </div>
-          <div className="flex gap-10">
-            <Input
-              title="Username"
-              type="text"
-              placeholder="Type a username"
-              name="username"
-              classNameVar="ml-[20px]"
-            />
-            <Input
-              title="Email"
-              type="text"
-              placeholder="Type your Email"
-              name="email"
-            />
-            <Input
-              title="Password"
-              type="password"
-              placeholder="Type a password"
-              name="password"
-              classNameVar="ml-[140px] mr-[20px]"
-            />
-          </div>
-          <div className="flex gap-10">
-            <Input
-              title="Country"
-              type="text"
-              placeholder="Select your country"
-              name="country"
-              classNameVar="ml-[20px]"
-            />
-            <Input
-              title="City"
-              type="text"
-              placeholder="Select your city"
-              name="city"
-            />
-            <SelectType
-              title="Account Type"
-              type="text"
-              options={optionsAcc}
-              name="accountType"
-              classNameVar="ml-[140px] mr-[20px]"
-            />
-          </div>
-
-          {/* Sign Up Button */}
+          {pageNumber === 1 && <SignUp1 />}
+          {pageNumber === 2 && <SignUp2 />}
+          {pageNumber === 3 && <SignUp3 />}
           <div className="flex flex-wrap justify-center pt-7.5">
             <div className="w-full block relative z-1 rounded-[25px] overflow-hidden">
               <button
+                type="button"
+                onClick={nextPageHandeler}
                 className={
                   "bg-blue-400 transition-colors duration-300 text-[16px] text-white flex justify-center items-center w-full h-[50px] border-0 cursor-pointer font-semibold hover:bg-blue-600"
                 }
               >
-                Create Account
+                Next
               </button>
             </div>
           </div>
