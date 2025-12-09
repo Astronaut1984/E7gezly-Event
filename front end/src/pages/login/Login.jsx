dd3c1a47678e738b62090aca93f772fe4cc646e5
bradocola <nagyomar91@gmail.com>
2025-12-09 12:31:12 +0200

    login done

--- front end/src/pages/login/Login.jsx
+++ front end/src/pages/login/Login.jsx
@@ -68,9 +68,9 @@ export default function Login() {
 
             {/* --- 1. DISPLAY ERROR MESSAGE HERE --- */}
             {error && (
-              <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative mb-4 text-center">
+              <span className="block text-sm text-[15px] text-red-500 text-center mb-8">
                 {error}
-              </div>
+              </span>
             )}
 
             <Input
