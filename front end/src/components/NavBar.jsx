1294566df24001a2029d13374d8a42452d33fc5a
Karim <karimnaderezzat@gmail.com>
2025-12-09 18:48:51 +0200

    navbar modified

--- front end/src/components/NavBar.jsx
+++ front end/src/components/NavBar.jsx
@@ -1,14 +1,20 @@
-import { Routes, NavLink, useLocation } from "react-router-dom";
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleTheme,setTheme } from "@/store/themeSlice";
+import { toggleTheme, setTheme } from "@/store/themeSlice";
+import { UserContext } from "@/UserContext";
 
 export default function NavBar() {
   const dispatch = useDispatch();
-  dispatch(setTheme(useSelector((state) => state.theme.dark)))
+  dispatch(setTheme(useSelector((state) => state.theme.dark)));
   const dark = useSelector((state) => state.theme.dark);
 
-  const location = useLocation();
+  const { user, setUser } = useContext(UserContext);
+
+  function handleLogout() {
+    // Clear user context on logout
+    setUser(null);
+  }
 
   return (
     <div className="z-10 fixed w-full bg-card text-primary items-center flex justify-between p-1">
@@ -21,8 +27,25 @@ export default function NavBar() {
         <NavBtn link="/Events" title="Events" />
       </nav>
       <nav className="mx-5 max-w-5xl flex items-center gap-6 py-4">
-        <NavBtn link="/login" title="Login" />
-        <NavBtn link="/signup" title="Sign up" />
+        {!user && <NavBtn link="/login" title="Login" />}
+        {!user && <NavBtn link="/signup" title="Sign up" />}
+        {user && (
+          <NavLink
+            to="/login"
+            onClick={handleLogout}
+            className="text-primary-hover"
+          >
+            Log out
+          </NavLink>
+        )}
+        {user && (
+          <NavLink
+            link="/profile"
+            className="bg-primary p-2 px-4 rounded-full text-white hover:bg-primary-hover"
+          >
+            {`${user?.first_name.charAt(0).toUpperCase()}`}
+          </NavLink>
+        )}
         <button
           onClick={() => dispatch(toggleTheme())}
           className="p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200"
